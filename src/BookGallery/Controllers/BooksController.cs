cce66eca0c798f5dcfe7f2c5767b0d6214a68a74
Lando B (Rolando Bernal) <jrolandobernal@gmail.com>
2016-09-16 11:27:37 -0500

    Added Controller and Detail action method

---
+++ src/BookGallery/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookGallery.Controllers
+{
+    public class BooksController : Controller
+    {
+        public string Detail()
+        {
+            return "Hello from the books controller";
+        }
+    }
+}
