8a345a7d3978b2538fc5be04c3aba2b70e1c705f
Lando B (Rolando Bernal) <jrolandobernal@gmail.com>
2016-09-19 09:47:18 -0500

    Updated book model

--- src/BookGallery/Models/Book.cs
+++ src/BookGallery/Models/Book.cs
@@ -13,5 +13,22 @@ namespace BookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        // series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
