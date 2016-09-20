73a44c5dd8e7364515dbbaa33dc9b77d346de0c9
Lando B (Rolando Bernal) <jrolandobernal@gmail.com>
2016-09-20 08:07:29 -0500

    Updated the default route

--- src/BookGallery/App_Start/RouteConfig.cs
+++ src/BookGallery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BookGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Books", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
