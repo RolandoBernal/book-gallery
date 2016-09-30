603649b006f5583e3f48a4fe10f7841b76edf5cf
Lando B (Rolando Bernal) <jrolandobernal@gmail.com>
2016-09-30 10:23:44 -0500

    Added Series List view

---
+++ src/BookGallery/Controllers/SeriesController.cs
@@ -0,0 +1,24 @@
+﻿using BookGallery.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookGallery.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private SeriesRepository _seriesRepository = null;
+
+        public SeriesController()
+        {
+            _seriesRepository = new SeriesRepository();
+        }
+        public ActionResult Index()
+        {
+            var series = _seriesRepository.GetSeries();
+            return View(series);
+        }
+    }
+}
